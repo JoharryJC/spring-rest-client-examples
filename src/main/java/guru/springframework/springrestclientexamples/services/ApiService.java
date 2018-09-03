77d0e5a8c489fff56660b11f79fce0fdd0cb74a6
JoharryJC <energycuenta@yahoo.com>
2018-09-03 10:54:01 -0400

    I am updating my project spring-rest-client (test and run is fine at the moment). I am refreshing my project.

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiService.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiService.java
@@ -1,6 +1,8 @@
 package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -9,4 +11,6 @@ Created by Joharry Correa 08/08/2018
  */
 public interface ApiService {
     List<User> getUsers(Integer limit);
+
+    Flux<User> getUsers(Mono<Integer> limit);
 }
